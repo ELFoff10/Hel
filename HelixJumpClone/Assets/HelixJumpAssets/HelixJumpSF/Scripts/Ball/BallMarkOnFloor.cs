c56979f818ed527fbbc8a7733469795cd14da79e
ELFoff10 <andrei.kaliadzin@gmail.com>
2022-10-24 19:28:19 +0300

    finish 2

---
+++ HelixJumpClone/Assets/HelixJumpAssets/HelixJumpSF/Scripts/Ball/BallMarkOnFloor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BallMarkOnFloor : BallEvents
+{
+    [SerializeField] private Mark markPrefab;
+    [SerializeField] private Transform generateTransformVisualModel;
+    [SerializeField] private Transform parentTransformLevel;
+
+    protected override void OnBallCollisionSegment(SegmentType type)
+    {
+        if (type != SegmentType.Empty)
+        {
+            Mark mark = Instantiate(markPrefab);
+
+            mark.SetRandomRotation();
+            mark.transform.position = generateTransformVisualModel.position;
+            mark.transform.SetParent(parentTransformLevel);
+            mark.transform.position -= new Vector3(0, 0.1f, 0);
+        }
+    }
+}
